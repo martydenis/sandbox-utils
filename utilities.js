eab6699cf1e04b51d4c48357af7f88ba36f026f3
martydenis <martin.2nis@gmail.com>
2024-09-01 15:06:16 +0200

    [IMP] Added useful easing functions

--- utilities.js
+++ utilities.js
@@ -94,9 +94,9 @@ export function randomValueFromArray (array) {
 }
 
 export function hexToRGB (hex, alpha) {
-    const r = parseInt(hex.slice(1, 3), 16),
-        g = parseInt(hex.slice(3, 5), 16),
-        b = parseInt(hex.slice(5, 7), 16);
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
 
     if (alpha) {
         return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
